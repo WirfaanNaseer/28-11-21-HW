b05565922a158a54dee2a35082328a812fc2f6e2
WirfaanNaseer <113512057+WirfaanNaseer@users.noreply.github.com>
2022-11-26 14:07:51 +0000

    Add files via upload

---
+++ DogYears.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DogYears
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your dogs age in human years > ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(dogYears(num1));
+        }
+        private static int dogYears(double humanYears)
+        {
+            int dogYearsOld = 0;
+            if (humanYears <= 2)
+            {
+                dogYearsOld = Convert.ToInt32(humanYears * 12);
+            }
+            else
+            {
+                dogYearsOld = Convert.ToInt32(24 + (6 * (humanYears - 2)));
+            }
+            return dogYearsOld;
+        }
+    }
+}
