c5577d1185dcb19061174487044fcba8870d2ade
WirfaanNaseer <113512057+WirfaanNaseer@users.noreply.github.com>
2022-11-26 17:22:47 +0000

    Update Divisibility.cs

--- Divisibility.cs
+++ Divisibility.cs
@@ -6,10 +6,13 @@ namespace Divisibility
     {
         static void Main(string[] args)
         {
+            //Get user inputs
             Console.Write("Input a number > ");
+            //Convert the number to integers
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Input another number > ");
             int num2 = Convert.ToInt32(Console.ReadLine());
+            //Put the two numbers into the subroutine and if they are divisible
             if (isDivisible(num1, num2))
             {
                 Console.WriteLine("The two numbers are divisible");
@@ -21,10 +24,12 @@ namespace Divisibility
         }
         private static bool isDivisible(int num1, int num2)
         {
+            //if the second number is 0 return false
             if (num2 == 0)
             {
                 return false;
             }
+            //using modulus check if there is a remainder when the two are divided
             if (num2 % num1 == 0)
             {
                 return true;
