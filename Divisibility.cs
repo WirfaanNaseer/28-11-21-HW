5b5b10a249d2d18462adeb65b44f2ad1124d8186
WirfaanNaseer <113512057+WirfaanNaseer@users.noreply.github.com>
2022-11-26 11:35:08 +0000

    Add files via upload

---
+++ Divisibility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Divisibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a number > ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input another number > ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (isDivisible(num1, num2))
+            {
+                Console.WriteLine("The two numbers are divisible");
+            }
+            else
+            {
+                Console.WriteLine("The two numbers are not divisible");
+            }
+        }
+        private static bool isDivisible(int num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                return false;
+            }
+            if (num2 % num1 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
